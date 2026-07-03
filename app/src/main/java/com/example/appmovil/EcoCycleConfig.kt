25ca2b0fe6ac5e534d3849360041c2bd0f700379
ZavalaUriel <zavalaurieloficial@gmail.com>
2026-07-02 19:04:25 -0600

    configure ecocycleconfig

--- app/src/main/java/com/example/appmovil/EcoCycleConfig.kt
+++ app/src/main/java/com/example/appmovil/EcoCycleConfig.kt
@@ -5,7 +5,7 @@ package com.example.appmovil
 
 object EcoCycleConfig {
     const val MACHINE_ID = "machine_001"
-    const val SERVER_HOST = "192.168.100.19"
+    const val SERVER_HOST = "10.43.13.239"
     const val NET_API_PORT = 5000
     val BASE_URL: String get() = "http://${SERVER_HOST}:${NET_API_PORT}/"
 }
